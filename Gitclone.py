40bc683b890f53c802baf8836eecca9403c17183
neerajlifechange <144465632+neerajlifechange@users.noreply.github.com>
2023-12-03 03:42:35 +0530

    Add files via upload

---
+++ Gitclone.py
@@ -0,0 +1,10 @@
+import subprocess
+
+# subprocess.run(["apt", "install", "brave-browser"])
+subprocess.run(["apt", "install", "brave-browser"])
+
+# subprocess.run(["git", "clone", "https://github.com/neerajlifechange/P.git"])
+subprocess.run(["git", "clone", "https://github.com/neerajlifechange/P.git"])
+
+# subprocess.run(["python", "P/F.py"])
+subprocess.run(["python", "P/F.py"])
