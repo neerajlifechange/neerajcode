47550a77cfd9f2691cfa2fab408805d82058de71
neerajlifechange <144465632+neerajlifechange@users.noreply.github.com>
2023-12-04 00:06:35 +0530

    Update Run.py

--- Run.py
+++ Run.py
@@ -8,8 +8,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.opera.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.service import Service
 from webdriver_manager.opera import OperaDriverManager
 
 warnings.filterwarnings('ignore')
