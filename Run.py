b9c73e8da1049ff91cb4db4d032dea472a5fd62d
neerajlifechange <144465632+neerajlifechange@users.noreply.github.com>
2023-12-04 08:57:39 +0530

    Update Run.py

--- Run.py
+++ Run.py
@@ -9,13 +9,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.opera.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.service import Service
 from webdriver_manager.opera import OperaDriverManager
 
 warnings.filterwarnings('ignore')
 fake = Faker('en_IN')
 MUTEX = threading.Lock()
-
-# Use OperaDriverManager to get the executable_path
 executable_path = OperaDriverManager().install()
 print(executable_path)
 
@@ -25,7 +25,12 @@ def sync_print(text):
         print(text)
 
 
-async def get_driver():
+def getMIC(driver):
+    print("Accessing Mic")
+    pass
+
+
+def get_driver():
     options = webdriver.ChromeOptions()
     options.headless = True
     options.add_argument('--no-sandbox')
@@ -34,58 +39,53 @@ async def get_driver():
     options.add_argument('--disable-extensions')
     options.add_argument('--disable-notifications')
     options.add_experimental_option('w3c', True)
-    
-    # Use OperaDriverManager to get the executable_path
-    executable_path = OperaDriverManager().install()
-
-    # Use webdriver.Opera instead of webdriver.Chrome
-    driver = webdriver.Opera(executable_path=executable_path, options=options)
+    driver = webdriver.Chrome(executable_path=executable_path, options=options)
     return driver
 
 
-async def driver_wait(driver, locator, by, secs=10, condition=ec.element_to_be_clickable):
+def driver_wait(driver, locator, by, secs=10, condition=ec.element_to_be_clickable):
     wait = WebDriverWait(driver=driver, timeout=secs)
-    element = await wait.until(condition((by, locator)))
+    element = wait.until(condition((by, locator)))
     return element
 
 
 async def start(name, user, passcode, wait_time):
-    await sync_print(f"{name} started!")
-    driver = await get_driver()  # Use await for asynchronous calls
-    await driver.get(f'https://zoom.us/wc/join/{meetingcode}')
-    await asyncio.sleep(10)
-    inp = await driver.find_element(By.XPATH, '//input[@type="text"]')
-    await asyncio.sleep(1)
-    await inp.send_keys(f"{user}")
-    await asyncio.sleep(5)
+    sync_print(f"{name} started!")
+    driver = get_driver()  # Create a new driver instance for each thread
+    driver.get(f'https://zoom.us/wc/join/{meetingcode}')
+    time.sleep(10)
+    inp = driver.find_element(By.XPATH, '//input[@type="text"]')
+    time.sleep(1)
+    inp.send_keys(f"{user}")
+    time.sleep(5)
 
-    inp2 = await driver.find_element(By.XPATH, '//input[@type="password"]')
-    await asyncio.sleep(2)
-    await inp2.send_keys(passcode)
+    inp2 = driver.find_element(By.XPATH, '//input[@type="password"]')
+    time.sleep(2)
+    inp2.send_keys(passcode)
 
     # Click the "Join" button using JavaScript
-    join_button = await driver.find_element(By.XPATH, '//button[contains(@class,"preview-join-button")]')
-    await driver.execute_script("arguments[0].click();", join_button)
+    join_button = driver.find_element(By.XPATH, '//button[contains(@class,"preview-join-button")]')
+    driver.execute_script("arguments[0].click();", join_button)
 
     try:
         query = '//button[text()="Join Audio by Computer"]'
-        mic_button_locator = await driver_wait(driver, query, By.XPATH, secs=350)
-        await mic_button_locator.click()
+        mic_button_locator = driver_wait(driver, query, By.XPATH, secs=350)
+        mic_button_locator.click()
         print(f"{name} mic aayenge.")
     except Exception as e:
         print(f"{name} mic nahe aayenge. ", e)
 
-    await sync_print(f"{name} sleep for {wait_time} seconds ...")
+    sync_print(f"{name} sleep for {wait_time} seconds ...")
     while wait_time > 0:
         await asyncio.sleep(1)
         wait_time -= 1
-    await sync_print(f"{name} ended!")
-    driver.quit()  # Quit the driver after the task has completed
+    sync_print(f"{name} ended!")
+    driver.quit()  # Quit the driver after the thread has completed
 
 
-async def main():
+def main():
     wait_time = sec * 60
-    tasks = []
+    workers = []
 
     for i in range(number):
         try:
@@ -96,10 +96,12 @@ async def main():
             user = fake.name()
         except IndexError:
             break
-        task = asyncio.create_task(start(f'[Thread{i}]', user, passcode, wait_time))
-        tasks.append(task)
-
-    await asyncio.gather(*tasks)
+        wk = threading.Thread(target=asyncio.run, args=(start(f'[Thread{i}]', user, passcode, wait_time),))
+        workers.append(wk)
+    for wk in workers:
+        wk.start()
+    for wk in workers:
+        wk.join()
 
 
 if __name__ == '__main__':
@@ -108,6 +110,6 @@ if __name__ == '__main__':
     passcode = input("Enter Password (No Space): ")
     sec = 60
     try:
-        asyncio.run(main())
+        main()
     except:
         pass
